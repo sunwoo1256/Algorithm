c8d8cd46917ae9ba1c7f8d3610ee4acee529b870
sunwoo1256 <117715215+sunwoo1256@users.noreply.github.com>
2024-11-23 17:19:57 +0900

    [Silver IV] Title: 스케이트 연습, Time: 0 ms, Memory: 2160 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+int main() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+
+	int n;
+	std::cin >> n;
+	std::vector<int> v(n+1, 0);
+	std::vector<int> ans(n+1, 1);
+
+	for (int i = 0; i < n; i++) {
+		std::cin >> v[i];
+	}
+	
+	int sum = 1;
+	for (int i = n - 2; i >= 0; i--) {
+		if (v[i] < ans[i + 1] + 1) {
+			ans[i] = v[i];
+		}
+		else {
+			ans[i] = ans[i + 1] + 1;
+		}
+		sum += ans[i];
+	}
+	std::cout << sum;
+}

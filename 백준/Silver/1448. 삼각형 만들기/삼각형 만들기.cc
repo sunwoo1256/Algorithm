2bcc22b9aa33cd03f624e5aa754f1d6e87f74ed1
sunwoo1256 <117715215+sunwoo1256@users.noreply.github.com>
2024-12-05 20:51:25 +0900

    [Silver III] Title: 삼각형 만들기, Time: 164 ms, Memory: 8296 KB -BaekjoonHub

--- 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.cc
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.cc
@@ -18,11 +18,9 @@ int main() {
 
 	std::sort(v.rbegin(), v.rend());
 	int sum = -1;
-	bool tri = false;
 	for (int i = 0; i < n - 2; i++) {
 		if (v[i] < v[i + 1] + v[i + 2]) {
 			sum = v[i] + v[i + 1] + v[i + 2];
-			tri = true;
 			break;
 		}
 

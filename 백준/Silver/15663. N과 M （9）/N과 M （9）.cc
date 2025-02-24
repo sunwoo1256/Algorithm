a3668e57fd2feb6542f5489c0d2883ddfe98b954
sunwoo1256 <117715215+sunwoo1256@users.noreply.github.com>
2025-02-25 02:41:45 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,23 +1,27 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
 int n, m;
+
 std::vector<int> v(9,0);
 std::vector<int> nums;
-std::vector<int> visited(10001, 0);
-std::vector<std::vector<int>> ans;
+std::vector<int> visited(9, 0);
 
 void dfs(int cnt) {
 	if (cnt == m) {
-		ans.push_back(v);
+		for (int i = 0; i < m; i++) {
+			std::cout << v[i] << " ";
+		}
+		std::cout << '\n';
 		return;
 	}
 
+	int last = 0;
 	for (int x = 0; x < nums.size(); x++) {
-		if (visited[x] == 0) {
+		if (visited[x] == 0 && last != nums[x]) {
 			v[cnt] = nums[x];
 			visited[x] = 1;
+			last = v[cnt];
 			dfs(cnt + 1);
 			visited[x] = 0;
 		}
@@ -38,14 +42,4 @@ int main() {
 	std::sort(nums.begin(), nums.end());
 
 	dfs(0);
-	std::sort(ans.begin(), ans.end());
-	auto newend = std::unique(ans.begin(), ans.end());
-	ans.erase(newend, ans.end());
-	
-	for (int i = 0; i < ans.size(); i++) {
-		for (int j = 0; j < m; j++) {
-			std::cout << ans[i][j] << " ";
-		}
-		std::cout << '\n';
-	}
 }

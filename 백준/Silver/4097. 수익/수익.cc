8fd8d72aa0b439d064c35f761c10e68a10d20aaf
sunwoo1256 <117715215+sunwoo1256@users.noreply.github.com>
2025-02-27 15:40:42 +0900

    [Silver II] Title: 수익, Time: 308 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/4097. 수익/수익.cc
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+int main() {
+	int n;
+	
+	while (std::cin >> n && n != 0) {
+		std::vector<int> v(250001, 0);
+		std::vector<int> dp(250001, -10001);
+		for (int i = 0; i < n; i++) {
+			int p;
+			std::cin >> p;
+			v[i] = p;
+		}
+		dp[0] = v[0];
+		for (int i = 1; i < n; i++) {
+			
+			dp[i] = std::max(dp[i - 1] + v[i], v[i]);
+			//std::cout<<i<<" " << dp[i] << '\n';
+		}
+		std::cout << *std::max_element(dp.begin(),dp.end()) << '\n';
+	}
+}
